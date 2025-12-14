7b6281116da40f3f3504d0969ae397f069a21a98
Nagarjunks <47100604+Nagarjunks@users.noreply.github.com>
2025-12-14 20:19:59 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,1 +1,16 @@
+from fastapi import FastAPI
+import joblib
+import numpy as np
 
+app = FastAPI()
+
+model = joblib.load("model.pkl")
+
+@app.post("/predict")
+def predict(features: list):
+    prediction = model.predict([features])
+    probability = model.predict_proba([features]).max()
+    return {
+        "prediction": int(prediction[0]),
+        "confidence": float(probability)
+    }
